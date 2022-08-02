0b7603e242f628a2386ea93c1ab1cfabde062d1d
jihaenglee <jmleeh819@gmail.com>
2022-08-03 00:11:51 +0900

    first commit

---
+++ src/main/java/com/example/limestudy/LimeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.limestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimeStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/limestudy/LimeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.limestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimeStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
